e7dd40955f114d43efcfcf3744921ec5614b2faf
balex <balexvicx@gmail.com>
2024-05-05 15:11:43 +0200

    Old getResult

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,8 +60,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
 
-    
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
